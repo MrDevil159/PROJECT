d2ef433d27a29d6e074ab76417d54e0955a918b8
Mehfooz Ahmed <mehfoozmaaz@gmail.com>
2023-07-27 10:54:20 +0530

    Main Recipe Project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent {
+
+recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'A Test Recipe Description','https://img.freepik.com/free-photo/double-hamburger-isolated-white-background-fresh-burger-fast-food-with-beef-cream-cheese_90220-1192.jpg?w=2000'),
+  new Recipe('A Test Recipe', 'A Test Recipe Description','https://img.freepik.com/free-photo/double-hamburger-isolated-white-background-fresh-burger-fast-food-with-beef-cream-cheese_90220-1192.jpg?w=2000'),
+
+];
+
+}
